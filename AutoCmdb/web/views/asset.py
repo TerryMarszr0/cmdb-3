2c9557cfa86784e1fe40e75d9972b20bc6ab5873
congzhang <652314341@qq.com>
2017-03-15 09:44:26 +0800

    配置新增资产页面

--- AutoCmdb/web/views/asset.py
+++ AutoCmdb/web/views/asset.py
@@ -5,8 +5,13 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import JsonResponse
 
+from django.forms import Form
+from django.forms import widgets
+from django.forms import fields
+
 from web.service import asset
-from utils.response import BaseResponse
+
+from repository import models
 
 class AssetListView(View):
     def get(self, request, *args, **kwargs):
@@ -34,12 +39,75 @@ class AssetDetailView(View):
         return render(request, 'asset_detail.html', {'response': response, 'device_type_id': device_type_id})
 
 
+class AddAssetForm(Form):
+    """桌台管理Form表单"""
+
+    device_type_id = fields.ChoiceField(
+        choices=models.Asset.device_type_choices,
+        widget=widgets.Select(
+            attrs={}
+        )
+
+    )
+    device_status_id = fields.ChoiceField(
+        choices=models.Asset.device_status_choices,
+        widget=widgets.Select
+
+    )
+
+    hostname = fields.CharField(
+        error_messages={
+            "required": "主机名不能为空",
+        },
+        widget=widgets.Input(
+            attrs={"class": "form-control", "name": "hostname", "type": "text"})
+    )
+
+    cabinet_num = fields.CharField(
+        required=False,
+        widget=widgets.Input(
+            attrs={"class": "form-control", "placeholder": "请输入机柜号,没有可为空", "name": "hostname", "type": "text"})
+    )
+
+    cabinet_order = fields.CharField(
+        required=False,
+        widget=widgets.Input(
+            attrs={"class": "form-control", "placeholder": "请输入机柜所在位置,没有可为空", "name": "hostname", "type": "text"})
+    )
+
+    idc = fields.ChoiceField(
+        choices=[],
+        widget=widgets.Select
+
+    )
+
+    business_unit = fields.ChoiceField(
+        choices=models.BusinessUnit.objects.values('id', 'name'),
+        widget=widgets.Select
+
+    )
+
+    tag = fields.CharField(
+        required=False,
+        widget=widgets.Input(
+            attrs={"class": "form-control", "placeholder": "请输入机柜号,没有可为空", "name": "hostname", "type": "checkbox"})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(AddAssetForm, self).__init__(*args, **kwargs)
+
+        values = models.IDC.objects.only('id', 'name', 'floor')
+        result = map(lambda x: {'id': x.id, 'name': "%s-%s" % (x.name, x.floor)}, values)
+        self.idc.choices = result
+
+
+
 class AddAssetView(View):
     def get(self, request, *args, **kwargs):
-        obj = asset.Asset()
-        response = obj.add_assets(request)
+        obj = AddAssetForm()
+
         referrer = request.META['HTTP_REFERER']
-        return render(request, 'add_asset.html', {'data': response.data, 'referrer': referrer})
+        return render(request, 'add_asset.html', {'obj': obj, 'referrer': referrer})
 
     def post(self, request, *args, **kwargs):
 
