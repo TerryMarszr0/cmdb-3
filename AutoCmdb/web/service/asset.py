b6fe1d166ebc9037aa574522f820e4669c2ea066
congzhang <652314341@qq.com>
2017-03-16 16:42:09 +0800

    配置资产管理编辑页面

--- AutoCmdb/web/service/asset.py
+++ AutoCmdb/web/service/asset.py
@@ -41,7 +41,7 @@ class Asset(BaseServiceList):
             {
                 'q': 'server_title',
                 'title': "主机名",
-                'display': 0,
+                'display': 1,
                 'text': {'content': "{n}", 'kwargs': {'n': '@server_title'}},
                 'attr': {}
             },
